9022605351ed4385f315a68b993306a87c225bf6
joel <joel.rod.roj@hotmail.com>
2022-03-03 22:16:54 -0600

    subiendo a aproduccion el envio del comprobante por correo

--- src/helpers/Urls.js
+++ src/helpers/Urls.js
@@ -1,9 +1,9 @@
 import configEnv from '../../config/configEnv';
 
 
-const BASE = configEnv.BASE || "http://localhost:5000";
+//const BASE = configEnv.BASE || "http://localhost:5000";
 //const BASE = process.env.URL_API || 'https://instituto-api-dev.herokuapp.com'
-//const BASE = process.env.URL_API || 'https://api-admin-paris.herokuapp.com';
+const BASE = process.env.URL_API || 'https://api-admin-paris.herokuapp.com';
 export const BRANCH = "2203-comprobante-correo";
 
 const CURSO = BASE + '/curso';
