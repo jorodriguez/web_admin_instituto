8f96c4d9bd5779fe219154c93e2bbd1479436420
Joel Rodriguez Rojas <jorodriguez@ihsa.mx>
2022-03-31 12:50:57 -0600

    cambios para subir al ambiente demo

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -4,5 +4,9 @@ const formatPrice = (value)=> {
     return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
   }
 
+  /*cambiar por esteexport const formatCurrency = (value) => {  
+     let val = (value / 1).toFixed(2).replace(',', '.')
+     return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");      
+ }*/
 
 export { formatPrice };
